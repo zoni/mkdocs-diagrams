41bd788b89407858afd52ca21f71f74df4229701
Nick Groenen <nick@groenen.me>
2020-09-04 10:27:36 +0200

    Release v1.0.0

--- CHANGES.md
+++ CHANGES.md
@@ -1,5 +1,12 @@
 # Changelog
 
+## v1.0.0
+
+- Use `sys.executable` when spawning python subprocesses.
+    - This is primarily a bugfix for Windows users, though other Operating Systems could also benefit.
+    - Special thanks to [Leonardo Monteiro](https://github.com/decastromonteiro) for helping pinpoint this issue.
+- With this plugin now being used successfully in a number of different environments, the version is bumped to 1.0 for a better stability promise.
+
 ## v0.0.2
 
 - `diagrams` added as a package dependency.
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(name):
 
 setuptools.setup(
     name="mkdocs-diagrams",
-    version="0.0.2",
+    version="1.0.0",
     packages=["mkdocs_diagrams"],
     url="https://github.com/zoni/mkdocs-diagrams",
     license="MIT",
@@ -21,7 +21,7 @@ setuptools.setup(
     install_requires=["mkdocs", "diagrams"],
     entry_points={"mkdocs.plugins": ["diagrams = mkdocs_diagrams:DiagramsPlugin",]},
     classifiers=[
-        "Development Status :: 4 - Beta",
+        "Development Status :: 5 - Production/Stable",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
